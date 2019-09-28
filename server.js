901cb0042ecaef49ca469417840cf590c033cdba
Melinda Santana <melinda.santana@gmail.com>
2019-09-27 19:12:55 -0700

    configuring questions and layout
    configuring questions and layout

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//making static assets
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'password',
+//   database : 'prezidents_db'
+// });
+ 
+// connection.connect();
+ 
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "/public/home.html"));
+});
+
+app.get('/survey', function(req, res){
+    res.sendFile(path.join(__dirname, "/public/survey.html"));
+})
+app.post('/survey', function(req, res){
+    console.log('someone submitted their survey')
+    console.log(req)
+})
+// by default the forms use req.query so let's not fight it
+//localhost:3000/insert?pres_name=justin
+// app.get('/insert', function(req, res){
+// 	// res.json(req.query);
+
+// 	if (req.query.pres_name.length > 1){
+// 		connection.query('INSERT INTO presidents (pres_name) VALUES (?)', [req.query.pres_name], function (error, results, fields) {
+// 		  if (error) res.send(error)
+// 		  else res.redirect('/');
+// 		});
+// 	}else{
+// 		res.send('invalid name')
+// 	}
+// });
+
+
+app.listen(3000, function(){
+	console.log('listening on 3000');
+});
